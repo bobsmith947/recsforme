d5252dfbc5bf793cca4e1060ed5787c00dc031aa
Kitaev <lkitaev@cherrycreekschools.org>
2018-03-09 12:09:37 -0700

    add RecBean

---
+++ src/java/me/recsfor/engine/recommend/RecBean.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2018 Lucas Kitaev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.recsfor.engine.recommend;
+
+import java.beans.*;
+import java.io.Serializable;
+
+/**
+ * JavaBeans component to assist with recommendation generation.
+ * @author lkitaev
+ */
+public class RecBean implements Serializable {
+  private static final long serialVersionUID = 8208767349207251619L;
+  public static final String PROP_NAME = "name";
+  public static final String PROP_ID = "id";
+  public static final String PROP_TYPE = "type";
+  public static final String PROP_LIKE = "like";
+  private String name;
+  private String id;
+  private String type;
+  private boolean like;
+  private final PropertyChangeSupport propertySupport;
+  
+  public RecBean() {
+    propertySupport = new PropertyChangeSupport(this);
+    name = "";
+    id = "";
+    like = false;
+  }
+  
+  public void addPropertyChangeListener(PropertyChangeListener listener) {
+    propertySupport.addPropertyChangeListener(listener);
+  }
+  
+  public void removePropertyChangeListener(PropertyChangeListener listener) {
+    propertySupport.removePropertyChangeListener(listener);
+  }
+  /**
+   * @return the id
+   */
+  public String getId() {
+    return id;
+  }
+  /**
+   * @param value the value to set
+   */
+  public void setId(String value) {
+    String oldValue = id;
+    id = value;
+    propertySupport.firePropertyChange(PROP_ID, oldValue, id);
+  }
+  /**
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
+  /**
+   * @param name the name to set
+   */
+  public void setName(String name) {
+    java.lang.String oldName = this.name;
+    this.name = name;
+    propertySupport.firePropertyChange(PROP_NAME, oldName, name);
+  }
+  /**
+   * @return the type
+   */
+  public String getType() {
+    return type;
+  }
+  /**
+   * @param type the type to set
+   */
+  public void setType(String type) {
+    java.lang.String oldType = this.type;
+    this.type = type;
+    propertySupport.firePropertyChange(PROP_TYPE, oldType, type);
+  }
+  /**
+   * @return the like
+   */
+  public boolean isLike() {
+    return like;
+  }
+  /**
+   * @param like the like to set
+   */
+  public void setLike(boolean like) {
+    boolean oldLike = this.like;
+    this.like = like;
+    propertySupport.firePropertyChange(PROP_LIKE, oldLike, like);
+  }
+}
