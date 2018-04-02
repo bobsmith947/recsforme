9a5646f63fe0dcedd064bb8941b9d49b5a356801
Lucas Kitaev <lkitaev@cherrycreekschools.org>
2018-04-02 15:24:49 -0600

    Update CredentialEncryption.java

--- src/java/me/recsfor/app/CredentialEncryption.java
+++ src/java/me/recsfor/app/CredentialEncryption.java
@@ -16,17 +16,54 @@
 package me.recsfor.app;
 
 import java.security.MessageDigest;
-//import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
-public class CredentialEncryption {
-  private String pass;
-  private String salt;
+public class CredentialEncryption() {
+  private final String PASS;
+  private final byte[] SALT;
   private String hash;
+  private boolean valid;
+  private static final String HASH_ALGO = "PBKDF2WithHmacSHA1";
+  private static final String SALT_ALGO = "SHA1PRNG";
+  private static final short ITERATIONS = 20000;
   
-  public CredentialEncryption {
-    pass = "";
-    salt = "";
+  public CredentialEncryption() {
+    PASS = "";
+    SALT = null;
     hash = "";
+    valid = false;
+  }
+  
+  public CredentialEncryption(String pass) {
+    PASS = pass;
+    SALT = generateSalt();
+    hash = generateHash();
+    valid = true;
+  }
+  
+  public CredentialEncryption(String pass, String salt, String hash) {
+    PASS = pass;
+    SALT = salt.getBytes();
+    this.hash = generateHash();
+    valid = validateHash(hash);
+  }
+  
+  private byte[] generateSalt() {
+    SecureRandom rand = SecureRandom.getInstance(SALT_ALGO);
+    byte[] salt = new byte[16];
+    rand.nextBytes(salt);
+    return salt;
+  }
+  
+  private String generateHash() {
+    char[] pass = PASS.toCharArray();
+    PBEKeySpec spec = new PBEKeySpec(pass, SALT, ITERATIONS);
+    SecretKeyFactory key = SecretKeyFactory.getInstance(HASH_ALGO);
+    byte[] enc = key.generateSecret(spec).getEncoded();
+    return ITERATIONS + ":" + SALT + ":" + enc;
   }
 }
