811516ebfd368d52b54d09efc6f8847650907139
Lucas Kitaev <lkitaev@cherrycreekschools.org>
2018-04-03 09:10:55 -0600

    Update CredentialEncryption.java

--- src/java/me/recsfor/app/CredentialEncryption.java
+++ src/java/me/recsfor/app/CredentialEncryption.java
@@ -22,21 +22,24 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
-
+/**
+ * This class is used to encrypt user credentials (their password), so it can be securely stored in a database.
+ * @author lkitaev
+ */
 public class CredentialEncryption {
-  private final String PASS;
-  private final String SALT;
-  private final String HASH;
+  private final String pass;
+  private final String salt;
+  private final String hash;
   private static final String HASH_ALGO = "PBKDF2WithHmacSHA1";
   private static final String SALT_ALGO = "SHA1PRNG";
-  private static final short ITERATIONS = 20000;
+  private static int iterations = 100000;
   /**
-   * Default constructor creating an empty password hash with no salt.
+   * Default constructor creating an empty password with no hash or salt.
    */
   public CredentialEncryption() {
-    PASS = "";
-    SALT = null;
-    HASH = null;
+    pass = "";
+    salt = null;
+    hash = null;
   }
   /**
    * Constructor to create a hash for the provided password using a newly generated salt.
@@ -46,9 +49,9 @@ public class CredentialEncryption {
    * @throws DecoderException if a hex string could not be converted
    */
   public CredentialEncryption(String pass) throws NoSuchAlgorithmException, InvalidKeySpecException, DecoderException {
-    PASS = pass;
-    SALT = generateSalt();
-    HASH = generateHash();
+    this.pass = pass;
+    salt = generateSalt();
+    hash = generateHash();
   }
   /**
    * Constructor to create a hash for the provided password using a previously generated salt.
@@ -59,20 +62,20 @@ public class CredentialEncryption {
    * @throws DecoderException if the salt could not be converted
    */
   public CredentialEncryption(String pass, String salt) throws NoSuchAlgorithmException, InvalidKeySpecException, DecoderException {
-    PASS = pass;
-    SALT = salt;
-    this.HASH = generateHash();
+    this.pass = pass;
+    this.salt = salt;
+    hash = generateHash();
   }
   /**
    * Creates a random 128-bit (32 character) salt to use for hashing.
-   * @return the salt
+   * @return the new salt
    * @throws NoSuchAlgorithmException if there is no suitable random number generator
    */
   private String generateSalt() throws NoSuchAlgorithmException {
     SecureRandom rand = SecureRandom.getInstance(SALT_ALGO);
-    byte[] salt = new byte[16];
-    rand.nextBytes(salt);
-    return Hex.encodeHexString(salt);
+    byte[] randSalt = new byte[16];
+    rand.nextBytes(randSalt);
+    return Hex.encodeHexString(randSalt);
   }
   /**
    * Creates a 256-bit (64 character) hash using password-based encryption.
@@ -82,8 +85,8 @@ public class CredentialEncryption {
    * @throws DecoderException if the salt could not be converted
    */
   private String generateHash() throws NoSuchAlgorithmException, InvalidKeySpecException, DecoderException {
-    char[] pass = PASS.toCharArray();
-    PBEKeySpec spec = new PBEKeySpec(pass, Hex.decodeHex(SALT.toCharArray()), ITERATIONS, 256);
+    char[] charPass = pass.toCharArray();
+    PBEKeySpec spec = new PBEKeySpec(charPass, Hex.decodeHex(salt.toCharArray()), iterations, 256);
     SecretKeyFactory key = SecretKeyFactory.getInstance(HASH_ALGO);
     byte[] enc = key.generateSecret(spec).getEncoded();
     return Hex.encodeHexString(enc);
@@ -100,8 +103,8 @@ public class CredentialEncryption {
    */
   public static boolean validatePassword(String testPass, String storedHash, String storedSalt) 
           throws NoSuchAlgorithmException, InvalidKeySpecException, DecoderException {
-    CredentialEncryption cred = new CredentialEncryption(testPass, storedSalt);
-    byte[] testHash = Hex.decodeHex(cred.generateHash().toCharArray());
+    CredentialEncryption testCred = new CredentialEncryption(testPass, storedSalt);
+    byte[] testHash = Hex.decodeHex(testCred.generateHash().toCharArray());
     byte[] hash = Hex.decodeHex(storedHash.toCharArray());
     int diff = hash.length ^ testHash.length;
     for (int i = 0; i < hash.length && i < testHash.length; i++) {
@@ -110,21 +113,27 @@ public class CredentialEncryption {
     return diff == 0;
   }
   /**
-   * @return the PASS
+   * @return the pass
    */
-  protected String getPASS() {
-    return PASS;
+  protected String getPass() {
+    return pass;
   }
   /**
-   * @return the SALT
+   * @return the salt
    */
-  public String getSALT() {
-    return SALT;
+  public String getSalt() {
+    return salt;
   }
   /**
-   * @return the HASH
+   * @return the hash
    */
-  public String getHASH() {
-    return HASH;
+  public String getHash() {
+    return hash;
+  }
+  /**
+   * @return the iterations
+   */
+  public static int getIterations() {
+    return iterations;
   }
 }
