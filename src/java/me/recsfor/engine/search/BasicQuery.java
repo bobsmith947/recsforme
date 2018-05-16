959b41d6089ba6c057a26bfb6cc586b63a9332e1
bobsmith947 <lkitaev@cherrycreekschools.org>
2018-05-15 19:36:20 -0600

    don't replace characters in query
    wasn't actually useful

--- src/java/me/recsfor/engine/search/BasicQuery.java
+++ src/java/me/recsfor/engine/search/BasicQuery.java
@@ -41,8 +41,9 @@ public class BasicQuery {
   }
 
   protected BasicQuery(String query) {
-    String replace = query.replaceAll("[;/?:@=&]", " ");
-    this.query = replace;
+    //String replace = query.replaceAll("[;/?:@=&]", " ");
+    //this.query = replace;
+    this.query = query;
     results = new LinkedHashMap<>();
   }
   
