6142dbe2f7c07e21febf039e0a5f74dcca5b23f6
Lucas Kitaev <lkitaev@mymail.mines.edu>
2019-11-16 19:35:03 -0700

    Fill in artist tables

--- MusicTables.sql
+++ MusicTables.sql
@@ -5,42 +5,75 @@
  * Uses PostgreSQL syntax.
 */
 
--- drop any existing tables
-DROP TABLE IF EXISTS
-	artist,
-	artist_type,
-	artist_credit,
-	artist_credit_name,
-	gender,
-	release,
-	release_group,
-	release_group_primary_type,
-	medium,
-	medium_format,
-	track,
-	recording
-CASCADE;
+-- create artist tables/indexes
 
--- create artist tables
-CREATE TABLE artist (
-	-- TODO
+CREATE TABLE IF NOT EXISTS artist_type (
+	    id                  INTEGER PRIMARY KEY,
+	    name                VARCHAR NOT NULL,
+	    parent              INTEGER REFERENCES artist_type (id),
+	    child_order         INTEGER NOT NULL DEFAULT 0,
+	    description         VARCHAR,
+	    gid                 UUID NOT NULL
 );
 
-CREATE TABLE artist_type (
-	-- TODO
+CREATE UNIQUE INDEX IF NOT EXISTS artist_type_idx_gid ON artist_type (gid);
+
+CREATE TABLE IF NOT EXISTS artist_credit (
+	    id                  INTEGER PRIMARY KEY,
+	    name                VARCHAR NOT NULL,
+	    artist_count        SMALLINT NOT NULL,
+	    ref_count           INTEGER DEFAULT 0,
+	    created             TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+	    edits_pending       INTEGER NOT NULL DEFAULT 0
 );
 
-CREATE TABLE artist_credit (
-	-- TODO
+CREATE TABLE IF NOT EXISTS gender (
+	    id                  INTEGER PRIMARY KEY,
+	    name                VARCHAR NOT NULL,
+	    parent              INTEGER REFERENCES gender (id),
+	    child_order         INTEGER NOT NULL DEFAULT 0,
+	    description         VARCHAR,
+	    gid                 UUID NOT NULL
 );
 
-CREATE TABLE artist_credit_name (
-	-- TODO
+CREATE UNIQUE INDEX IF NOT EXISTS gender_idx_gid ON gender (gid);
+
+CREATE TABLE IF NOT EXISTS artist (
+	    id                  INTEGER PRIMARY KEY,
+	    gid                 UUID NOT NULL,
+	    name                VARCHAR NOT NULL,
+	    sort_name           VARCHAR NOT NULL,
+	    begin_date_year     SMALLINT,
+	    begin_date_month    SMALLINT,
+	    begin_date_day      SMALLINT,
+	    end_date_year       SMALLINT,
+	    end_date_month      SMALLINT,
+	    end_date_day        SMALLINT,
+	    type                INTEGER REFERENCES artist_type (id),
+	    area                INTEGER, -- not used
+	    gender              INTEGER REFERENCES gender (id),
+	    comment             VARCHAR,
+	    edits_pending       INTEGER DEFAULT 0,
+	    last_updated        TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+	    ended               BOOLEAN DEFAULT FALSE,
+	    begin_area          INTEGER, -- not used
+	    end_area            INTEGER -- not used
 );
 
-CREATE TABLE gender (
-	-- TODO
+CREATE UNIQUE INDEX IF NOT EXISTS artist_idx_gid ON artist (gid);
+CREATE INDEX IF NOT EXISTS artist_idx_name ON artist (name);
+CREATE INDEX IF NOT EXISTS artist_idx_sort_name ON artist (sort_name);
+
+CREATE TABLE IF NOT EXISTS artist_credit_name (
+	    artist_credit       INTEGER REFERENCES artist_credit (id) ON DELETE CASCADE,
+	    position            SMALLINT NOT NULL,
+	    artist              INTEGER NOT NULL REFERENCES artist (id) ON DELETE CASCADE,
+	    name                VARCHAR NOT NULL,
+	    join_phrase		VARCHAR NOT NULL,
+	    PRIMARY KEY (artist_credit, position)
 );
+
+CREATE INDEX IF NOT EXISTS artist_credit_name_idx_artist ON artist_credit_name (artist);
 
 -- create release/release group tables
 CREATE TABLE release (
