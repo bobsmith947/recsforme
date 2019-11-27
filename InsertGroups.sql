2ed5c5b65328e8f7a4803cdbfa757916d72c515f
bobsmith947 <lucaskitaev@gmail.com>
2019-11-26 18:57:06 -0700

    Create InsertGroups.sql

---
+++ InsertGroups.sql
@@ -0,0 +1,23 @@
+/* 
+ * Copyright 2019 Lucas Kitaev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+-- insert artist groups
+INSERT INTO groups (gid, type)
+SELECT gid, 'artist' FROM artist;
+
+-- insert album groups
+INSERT INTO groups (gid, type)
+SELECT gid, 'album' FROM release_group;
