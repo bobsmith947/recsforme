32cafbd7cf5771f26cf8c3bf75d90659340a63c8
Lucas Kitaev <lkitaev@mymail.mines.edu>
2021-04-27 19:31:50 -0600

    Generate artist recommendations

--- src/py/recsforme.py
+++ src/py/recsforme.py
@@ -11,7 +11,7 @@ albums = getAlbums()
 artistLikes = np.full((len(users), len(artists)), np.nan, np.half)
 albumLikes = np.full((len(users), len(albums)), np.nan, np.half)
 
-# fill in album likes/dislikes
+# fill in likes/dislikes for each user
 for i in range(len(users)):
 	for j in range(len(albums)):
 		liked = users[i].groups.get(albums[j].id)
@@ -20,23 +20,38 @@ for i in range(len(users)):
 				albumLikes[i][j] = 1.0
 			else:
 				albumLikes[i][j] = -1.0
+	for j in range(len(artists)):
+		liked = users[i].groups.get(artists[j].id)
+		if liked is not None:
+			if liked is True:
+				artistLikes[i][j] = 1.0
+			else:
+				artistLikes[i][j] = -1.0
 
-# create missing mask
+# create mask for likes/dislikes
 albumMask = np.ma.make_mask(~np.isnan(albumLikes))
+artistMask = np.ma.make_mask(~np.isnan(artistLikes))
 
 # fill in missing entries
+# mask out the entries that have already been filled in
 swa = SimilarityWeightedAveraging(verbose=True)
 albumRecs = np.ma.MaskedArray(swa.fit_transform(albumLikes),
 				albumMask, fill_value=np.NINF)
+artistRecs = np.ma.MaskedArray(swa.fit_transform(artistLikes),
+				artistMask, fill_value=np.NINF)
 
-# find top picks
+# find top picks and add them to each user's recommendations
 numPicks = 10
 for i in range(len(users)):
 	users[i].recs = {}
 	topAlbums = albumRecs[i].argsort(endwith=False)[-numPicks:]
+	topArtists = artistRecs[i].argsort(endwith=False)[-numPicks:]
 	for j in topAlbums:
 		if albumRecs[i][j] > 0:
 			users[i].recs[albums[j]] = albumRecs[i][j]
+	for j in topArtists:
+		if artistRecs[i][j] > 0:
+			users[i].recs[artists[j]] = artistRecs[i][j]
 
 updateUserRecs(users)
 
