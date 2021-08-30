e09d2395352ae0a35a52c2de8cb294952dd78031
Lucas Kitaev <lkitaev@mymail.mines.edu>
2021-08-30 03:33:20 -0600

    Update copy scripts

--- CopySample.sql
+++ CopySample.sql
@@ -1,12 +1,11 @@
 /*
- * Copies data from a full MusicBrainz database dump.
+ * Copies data from a sample MusicBrainz database dump.
  * Downloads are available from: http://ftp.musicbrainz.org/pub/musicbrainz/data/
  */
 
 \cd
-\! curl "ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/LATEST" | curl -O "ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/`cat`/mbdump{,-derived}.tar.bz2"
-\! tar -xf mbdump.tar.bz2 mbdump/artist mbdump/artist_credit mbdump/artist_credit_name mbdump/artist_type mbdump/gender mbdump/medium mbdump/release mbdump/release_group mbdump/release_group_primary_type mbdump/release_group_secondary_type mbdump/release_group_secondary_type_join mbdump/track
-\! tar -xf mbdump-derived.tar.bz2 mbdump/release_meta mbdump/release_group_meta
+\! curl "ftp.musicbrainz.org/pub/musicbrainz/data/sample/LATEST" | curl -O "ftp.musicbrainz.org/pub/musicbrainz/data/sample/`cat`/mbdump-sample.tar.xz"
+\! tar -xf mbdump-sample.tar.xz mbdump/artist mbdump/artist_credit mbdump/artist_credit_name mbdump/artist_type mbdump/gender mbdump/medium mbdump/release mbdump/release_coverart mbdump/release_group mbdump/release_group_meta mbdump/release_group_primary_type mbdump/release_group_secondary_type mbdump/release_group_secondary_type_join mbdump/track
 \cd mbdump
 \copy artist_type from 'artist_type'
 \copy artist_credit from 'artist_credit'
@@ -19,6 +18,6 @@
 \copy release_group_secondary_type_join from 'release_group_secondary_type_join'
 \copy release_group_meta from 'release_group_meta'
 \copy release from 'release'
-\copy release_coverart from 'release_meta'
+\copy release_coverart from 'release_coverart'
 \copy medium from 'medium'
 \copy track from 'track'
